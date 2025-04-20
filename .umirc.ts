8d332b7935c76fcd04d212610a39f1eafda79a69
vanness30214 <vanness@vip.qq.com>
2025-04-21 04:52:30 +0800

    完善配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "umi";
 
+
 export default defineConfig({
-  base:process.env.NODE_ENV==="production"?'/vanness30214':"/",
+  base:process.env.NODE_ENV==="production"?'/virtual_leave_form/':"/",
+  publicPath:process.env.NODE_ENV==="production"?'/virtual_leave_form/':"/",
   routes: [
     { path: "/", component: "index" }
   ],
-  outputPath:'build',
+  outputPath:'build/virtual_leave_form',
   npmClient: 'pnpm',
+  hash:true,
+  copy: [ { from: 'CNAME', to: 'build' },]
 });
