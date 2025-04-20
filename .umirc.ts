23f65fe700910635980115d3299be733817bea78
vanness30214 <vanness@vip.qq.com>
2025-04-21 03:24:39 +0800

    完善配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  base:process.env.NODE_ENV==="production"?'/vanness30214':"/",
   routes: [
     { path: "/", component: "index" }
   ],
+  outputPath:'build',
   npmClient: 'pnpm',
 });
